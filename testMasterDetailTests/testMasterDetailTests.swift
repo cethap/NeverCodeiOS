8ed033a577d2ac04531c5b0b25b8b168e6284577
Cesar Augusto Tapasco Arias <cesartapasco@MacBook-Pro-de-Cesar.local>
2019-04-29 15:31:46 -0500

    first commit

---
+++ testMasterDetailTests/testMasterDetailTests.swift
@@ -0,0 +1,34 @@
+//
+//  testMasterDetailTests.swift
+//  testMasterDetailTests
+//
+//  Created by Cesar Augusto Tapasco Arias on 4/29/19.
+//  Copyright © 2019 Cesar Augusto Tapasco Arias. All rights reserved.
+//
+
+import XCTest
+@testable import testMasterDetail
+
+class testMasterDetailTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
